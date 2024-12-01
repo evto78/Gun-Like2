31204a4aa0c046f79c1f276a939cb76ae5374fba
evto78 <150751428+evto78@users.noreply.github.com>
2024-12-01 16:47:06 -0500

    inventory progress

--- GunLike2/Assets/scripts/UI/InventoryScript.cs
+++ GunLike2/Assets/scripts/UI/InventoryScript.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class InventoryScript : MonoBehaviour
 {
     GameObject player;
+    public GameObject item;
 
     public List<Vector4> leftInventory;
     public List<Vector4> rightInventory;
 
+    List<Sprite> itemSprites;
+
+    public GameObject row;
+    public List<GameObject> rows;
+
     private void Start()
     {
         player = GameObject.Find("Player");
+
+        itemSprites = item.GetComponent<Item>().spriteList;
     }
 
     public void ArrangeInventory()
