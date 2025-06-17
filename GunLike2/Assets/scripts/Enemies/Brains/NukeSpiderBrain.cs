34f3e0c9f9f2e42b00b53a6c2fcd44d44d5f5257
evto78 <150751428+evto78@users.noreply.github.com>
2025-06-17 16:19:27 -0400

    Nuclear explosion and Nuclear spider added

---
+++ GunLike2/Assets/scripts/Enemies/Brains/NukeSpiderBrain.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NukeSpiderBrain : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+    public float jumpDistance;
+    public float jumpHeight;
+    NavAI nav;
+    NavMeshAgent agent;
+    public List<GameObject> legs;
+    bool jumping;
+    bool diving;
+    Vector3 divingAngle;
+    public GameObject nuke;
+    public float nukeDmg;
+    public GameObject tail;
+    public GameObject shine;
+    
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody>();
+        nav = GetComponent<NavAI>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!jumping && Vector3.Distance(player.transform.position, transform.position) < jumpDistance)
+        {
+            jumping = true;
+            nav.enabled = false;
+            agent.enabled = false;
+            Jump();
+        }
+        nav.enabled = !jumping;
+        agent.enabled = !jumping;
+        tail.SetActive(jumping);
+        shine.SetActive(diving);
+        if(jumping && rb.velocity.y < 0 && !diving)
+        {
+            diving = true;
+            Dive();
+        }
+        if (diving)
+        {
+            transform.eulerAngles = divingAngle;
+        }
+        
+    }
+    void Jump()
+    {
+        transform.LookAt(transform.position + Vector3.up);
+        transform.position += Vector3.up * 6f;
+        rb.velocity = Vector3.up * jumpHeight;
+        foreach(GameObject leg in legs)
+        {
+            leg.SetActive(false);
+        }
+    }
+    void Dive()
+    {
+        rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        transform.LookAt(player.transform);
+        divingAngle = transform.eulerAngles;
+        rb.AddForce((player.transform.position - transform.position).normalized * 500f, ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if((collision.gameObject.tag == "Player" || collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Untagged") && diving)
+        {
+            GameObject spawnedNuke = Instantiate(nuke);
+            spawnedNuke.transform.position = transform.position;
+            spawnedNuke.GetComponent<NuclearExplosion>().damage = nukeDmg;
+            Destroy(gameObject);
+        }
+    }
+    private void OnDisable()
+    {
+        nav.enabled = false;
+    }
+    private void OnEnable()
+    {
+        nav.enabled = true;
+    }
+}
---
+++ GunLike2/Assets/scripts/Item/NuclearExplosion.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuclearExplosion : MonoBehaviour
+{
+    float explosionTimer;
+    public float damage;
+    public float radiasMult;
+    public float lifetime;
+    public float approachRate;
+    bool done;
+    Light brightthingy;
+    public AnimationCurve lightCurve;
+
+    SphereCollider myCollider;
+    void Start()
+    {
+        myCollider = GetComponent<SphereCollider>();
+        brightthingy = GetComponentInChildren<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        explosionTimer += Time.deltaTime * approachRate;
+        brightthingy.intensity = lightCurve.Evaluate(explosionTimer / (lifetime * approachRate)) * 60f;
+        if (myCollider != null) { myCollider.radius = explosionTimer * radiasMult; }
+        if (explosionTimer > lifetime * approachRate && !done) { Destroy(myCollider); Destroy(gameObject, 30f); done = true; }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyWeakPoint")
+        {
+            if (collision.transform.parent.TryGetComponent<EnemyHealthManager>(out EnemyHealthManager ehm))
+            {
+                ehm.TakeDamage(damage, false, "normalHit", collision.gameObject.transform.position, "self");
+            }
+            else if (collision.TryGetComponent<EnemyHealthManager>(out EnemyHealthManager ehm2))
+            {
+                ehm2.TakeDamage(damage, false, "normalHit", collision.gameObject.transform.position, "self");
+            }
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.GetComponent<HealthManager>().TakeDamage(damage, false);
+        }
+    }
+}
