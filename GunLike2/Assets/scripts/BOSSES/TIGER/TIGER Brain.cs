85cd980b7711c4e8476c5e693758672331a56329
evto78 <150751428+evto78@users.noreply.github.com>
2025-11-20 00:30:07 -0500

    working on head turning

--- GunLike2/Assets/scripts/BOSSES/TIGER/TIGER Brain.cs
+++ GunLike2/Assets/scripts/BOSSES/TIGER/TIGER Brain.cs
@@ -6,13 +6,17 @@ using UnityEngine.AI;
 
 public class TIGERBrain : MonoBehaviour
 {
-    // Refrences
+    [Header("References")]
     TIGERBodyHeightAdjust bha;
     TIGERTailManager tm;
     TIGERNavAI nav;
     NavMeshAgent agent;
     Transform player;
     GameDataManager gdm;
+    public Animator headAnim;
+    public Transform headPointer;
+    public Transform cannonFirepoint;
+
     [Header("States and Stats")]
     public Vector2 baseWalkSpeedAccel;
     public Vector2 baseChaseSpeedAccel;
@@ -25,6 +29,12 @@ public class TIGERBrain : MonoBehaviour
     float backAccel = 2; float curBackSpeed = 0; float backstepTimer = 0;
     float followRotSpeed = 2f;
     public AnimationCurve sineCurve;
+    //HeadMovement
+    Vector2 manualCamInputDir; Vector2 manualCamDir = Vector2.zero;
+    public List<Transform> headJoints;
+    List<float> downXVals; List<float> upXVals; List<float> midXVals;
+    List<float> leftYVals; List<float> rightYVals; List<float> midYVals;
+    float prevX = 0f; float prevY = 0f;
 
     private void Awake()
     {
@@ -34,10 +44,22 @@ public class TIGERBrain : MonoBehaviour
         tm = GetComponentInChildren<TIGERTailManager>();
         gdm = GameObject.FindGameObjectWithTag("gdm").GetComponent<GameDataManager>();
         player = gdm.phm.transform;
+
+        InitializeHeadJointVals();
     }
     void Start()
     {
         ChangeState(curState, curMoveState);
+    }
+    void InitializeHeadJointVals()
+    {
+        headAnim.enabled = false;
+        downXVals = new List<float>() { 10f, 12f, 3f, 3f, 3f, 3f, 3f, 3f, 3f, 3f, -12f, 0f, 0f };
+        midXVals = new List<float>() { 10f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
+        upXVals = new List<float>() { 10f, -40f, -5f, -5f, -15f, -15f, -5f, 0f, 10f, 10f, -40f, 0f, 0f };
+        leftYVals = new List<float>() { -20f, 0f, -5f, -5f, -5f, -5f, -10f, -10f, -5f, -5f, 30f, 0f, 0f };
+        midYVals = new List<float>() { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
+        rightYVals = new List<float>() { 20f, 0f, 5f, 5f, 5f, 5f, 10f, 10f, 5f, 5f, -30f, 0f, 0f };
     }
     void ChangeState(State newState, MoveState newMoveState) 
     { 
@@ -62,10 +84,18 @@ public class TIGERBrain : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha5)) { ChangeState(curState, MoveState.walk); }
         if (Input.GetKeyDown(KeyCode.Alpha6)) { ChangeState(curState, MoveState.chase); }
         if (Input.GetKeyDown(KeyCode.Alpha7)) { ChangeState(curState, MoveState.sprint); }
+
+        manualCamInputDir = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow)) { manualCamInputDir += Vector2.up; }
+        if (Input.GetKey(KeyCode.DownArrow)) { manualCamInputDir -= Vector2.up; }
+        if (Input.GetKey(KeyCode.LeftArrow)) { manualCamInputDir -= Vector2.right; }
+        if (Input.GetKey(KeyCode.RightArrow)) { manualCamInputDir += Vector2.right; }
+        manualCamDir += Time.deltaTime * manualCamInputDir;
+        manualCamDir = new Vector2(Mathf.Clamp(manualCamDir.x, -1, 1), Mathf.Clamp(manualCamDir.y, -1, 1));
     }
     void Update()
     {
-        //ManualInput();
+        ManualInput();
 
         if (curBackSpeed > 0) { curBackSpeed -= backAccel * Time.deltaTime; }
         if (backstepTimer > 0) { backstepTimer -= Time.deltaTime; }
@@ -84,6 +114,94 @@ public class TIGERBrain : MonoBehaviour
                 transform.rotation = Quaternion.Lerp(curRot, tarRot, Time.deltaTime * followRotSpeed);
                 break; }
         transform.position -= Time.deltaTime * sineCurve.Evaluate(curBackSpeed) * baseWalkSpeedAccel.x * 0.2f * transform.forward;
+
+        HeadMovement();
     }
     void CheckStoppedBackstep() { if (bha.currentSpeed < 0.2f && backstepTimer <= 0) { ChangeState(State.idle, curMoveState); } }
+    void HeadMovement()
+    {
+        float xAxis;
+        float yAxis;
+
+        headPointer.LookAt(player); //float outy = headPointer.localEulerAngles.y; float outx = -headPointer.localEulerAngles.x;
+        //if (outy > -60 && outy < 60 && outx > -60 && outx < 60)
+        //{
+        //    yAxis = outy / 20f;
+        //    if (outx > 0) { xAxis = outx / 12f; }
+        //    else { xAxis = outx / 40f; }
+        //}
+        //else
+        //{
+        //    xAxis = 0f; yAxis = 0f;
+        //}
+
+        //Debug.Log("Current: " + outx + " | " + outy);
+        //Debug.Log("Target: " + xAxis + " | " + yAxis);
+
+        //headPointer.LookAt(cannonFirepoint);
+        //xAxis = 0.7f;
+        //yAxis = -0.2f;
+
+        xAxis = Mathf.Lerp(prevX, xAxis, Time.deltaTime*4f);
+        yAxis = Mathf.Lerp(prevY, yAxis, Time.deltaTime*4f);
+
+        prevX = xAxis; prevY = yAxis;
+
+        //xAxis = manualCamDir.y;
+        //yAxis = manualCamDir.x;
+
+        Debug.DrawRay(cannonFirepoint.position, cannonFirepoint.forward * 20, Color.red);
+        Debug.DrawRay(headPointer.position, headPointer.forward * 20, Color.yellow);
+        switch (xAxis)
+        {
+            case > 0:
+                switch (yAxis)
+                {
+                    case > 0: HeadJointAdjust(xAxis, yAxis, midXVals, upXVals, midYVals, rightYVals); break;
+                    case 0: HeadJointAdjust(xAxis, yAxis, midXVals, upXVals, midYVals, midYVals); break;
+                    case < 0: HeadJointAdjust(xAxis, 1 + yAxis, midXVals, upXVals, leftYVals, midYVals); break;
+                }
+                break;
+            case 0:
+                switch (yAxis)
+                {
+                    case > 0: HeadJointAdjust(xAxis, yAxis, midXVals, midXVals, midYVals, rightYVals); break;
+                    case 0: HeadJointAdjust(xAxis, yAxis, midXVals, midXVals, midYVals, midYVals); break;
+                    case < 0: HeadJointAdjust(xAxis, 1 + yAxis, midXVals, midXVals, leftYVals, midYVals); break;
+                }
+                break;
+            case < 0:
+                switch (yAxis)
+                {
+                    case > 0: HeadJointAdjust(1 + xAxis, yAxis, downXVals, midXVals, midYVals, rightYVals); break;
+                    case 0: HeadJointAdjust(1 + xAxis, yAxis, downXVals, midXVals, midYVals, midYVals); break;
+                    case < 0: HeadJointAdjust(1 + xAxis, 1 + yAxis, downXVals, midXVals, leftYVals, midYVals); break;
+                }
+                break;
+        }
+    }
+    void HeadJointAdjust(float xAxis, float yAxis, List<float> xMin, List<float> xMax, List<float> yMin, List<float> yMax)
+    {
+        xAxis = Mathf.Abs(xAxis); yAxis = Mathf.Abs(yAxis);
+        if (xMin == xMax && yMin == yMax)
+        {
+            for (int i = 0; i < headJoints.Count; i++)
+            {
+                Vector3 newDir = Vector3.zero;
+                newDir += Vector3.right * xMin[i];
+                newDir += Vector3.up * yMin[i];
+                headJoints[i].localEulerAngles = newDir;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < headJoints.Count; i++)
+            {
+                Vector3 newDir = Vector3.zero;
+                newDir += Vector3.right * Mathf.Lerp(xMin[i], xMax[i], xAxis);
+                newDir += Vector3.up * Mathf.Lerp(yMin[i], yMax[i], yAxis);
+                headJoints[i].localEulerAngles = newDir;
+            }
+        }
+    }
 }
